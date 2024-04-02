fdf0573fd3244e74a85b6be6d89a6dd0b0b88c59
Ankitsaini111 <ankitsainikrj123@gmail.com>
2024-04-02 11:30:42 +0530

    update

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -1,14 +1,14 @@
-const sequelize = require('sequelize')
-const dbInstance = new sequelize('tesk_application', 'root', 'toor', {
-    host: 'localhost',
-    dialect: 'mysql',
-    operatorAliases: 0,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+const sequelize=require('sequelize')
+const dbInstance=new sequelize('task_application','root','toor',{
+    host:'localhost',
+    dialect:'mysql',
+    pool:{
+        operatorAliases:0,
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
     }
-})
+});
 
-module.exports = dbInstance;
+module.exports=dbInstance
