90ff042820924422808c131a19b70bc11bfeca12
JatinThakur022 <thakurjatinrajputana@gmail.com>
2024-04-02 10:47:58 -0400

    updates

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express=require("express")
-const app=express()
+const express = require("express")
+const app = express()
 
-const bodyParser=require("body-parser")
+const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 
-const cors=require("cors")
+const cors = require("cors")
 const serverConfig = require("./configs/serverConfig")
 app.use(cors())
 
-app.listen(serverConfig.PORT,()=>{
+app.listen(serverConfig.PORT, () => {
     console.log(`Server is running up and down by PORT: ${serverConfig.PORT}`);
 })
